f3ac48b84e3540cc9baa6bac223be75daa5d30aa
Piotr Milcarz <piotr.milcarz@gmail.com>
2021-04-01 11:37:42 +0200

    model fix

--- lib/models/build.rb
+++ lib/models/build.rb
@@ -8,7 +8,7 @@ require 'models/repository'
 # Build model
 class Build < Model
   belongs_to :repository
-  belongs_to :build_config, foreign_key: :config_id, dependent: :delete_all
+  belongs_to :build_config, foreign_key: :config_id, dependent: :delete
   has_many   :jobs, -> { order('id') }, foreign_key: :source_id, dependent: :delete_all, class_name: 'Job'
 
   self.table_name = 'builds'
