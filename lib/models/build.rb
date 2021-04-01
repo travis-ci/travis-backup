c8bd0b9c67b29404b91a2663456e1673a91b767a
Piotr Milcarz <piotr.milcarz@gmail.com>
2021-04-01 11:21:49 +0200

    Models optimizations

--- lib/models/build.rb
+++ lib/models/build.rb
@@ -8,8 +8,8 @@ require 'models/repository'
 # Build model
 class Build < Model
   belongs_to :repository
-  belongs_to :build_config, foreign_key: :config_id, dependent: :destroy
-  has_many   :jobs, -> { order('id') }, foreign_key: :source_id, dependent: :destroy, class_name: 'Job'
+  belongs_to :build_config, foreign_key: :config_id, dependent: :delete_all
+  has_many   :jobs, -> { order('id') }, foreign_key: :source_id, dependent: :delete_all, class_name: 'Job'
 
   self.table_name = 'builds'
 end
