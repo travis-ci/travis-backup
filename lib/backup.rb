4d9899147120aebf506a4ee5395747af8ad9275a
Piotr Milcarz <piotr.milcarz@gmail.com>
2021-04-12 18:58:43 +0200

    Debug

--- lib/backup.rb
+++ lib/backup.rb
@@ -61,6 +61,7 @@ class Backup
   end
 
   def process_repo(repository) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    puts "Processing repository #{repository.id}"
     repository.builds.where('created_at < ?', @config.delay.to_i.months.ago.to_datetime)
               .in_groups_of(@config.limit.to_i, false).map do |builds|
       if builds.count == @config.limit.to_i
@@ -68,6 +69,7 @@ class Backup
         file_name = "repository_#{repository.id}_builds_#{builds.first.id}-#{builds.last.id}.json"
         pretty_json = JSON.pretty_generate(builds_export)
         if upload(file_name, pretty_json)
+          puts "Uploaded file #{file_name}"
           BuildBackup.new(repository_id: repository.id, file_name: file_name).save!
           builds.each(&:destroy)
         end
