fa2b43514d4e5ccf0499fb0581e2fa08593a2c9e
Karol Selak <karol.selak@gmail.com>
2021-08-15 11:26:55 +0200

    housekeeping period for files removed from settings

--- lib/config.rb
+++ lib/config.rb
@@ -2,7 +2,7 @@
 
 # Config for travis-backup
 class Config
-  attr_reader :limit, :delay, :housekeeping_period, :database_url, :logs_url
+  attr_reader :limit, :delay, :database_url, :logs_url
 
   def initialize # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength
     config = YAML.load(File.open('config/settings.yml'))
@@ -11,7 +11,6 @@ class Config
     @limit = ENV['BACKUP_LIMIT'] || config['backup']['limit']
     @delay = ENV['BACKUP_DELAY'] || config['backup']['delay']
     @logs_url = ENV['LOGS_URL'] || config['backup']['logs_url']
-    @housekeeping_period = ENV['BACKUP_HOUSEKEEPING_PERIOD'] || config['backup']['housekeeping_period']
     @database_url = ENV['DATABASE_URL'] || connection_details['development']
   end
 end
