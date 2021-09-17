d3e142aa56c70c9bf14c83a7cec8d3b9787dd597
Karol Selak <karol.selak@gmail.com>
2021-09-17 17:06:42 +0200

    Backup::RemoveOrphans - refactoring wip

--- lib/travis-backup.rb
+++ lib/travis-backup.rb
@@ -16,6 +16,7 @@ require 'models/ssl_key'
 require 'models/request'
 require 'models/stage'
 require 'backup/move_logs'
+require 'backup/remove_orphans'
 
 # main travis-backup class
 class Backup
@@ -44,7 +45,7 @@ class Backup
     if @config.move_logs
       Backup::MoveLogs.new(@config, @db_helper, @dry_run_reporter).run
     elsif @config.remove_orphans
-      remove_orphans
+      Backup::RemoveOrphans.new(@config, @dry_run_reporter).run
     elsif user_id
       process_repos_for_owner(user_id, 'User')
     elsif org_id
@@ -99,136 +100,6 @@ class Backup
               .in_groups_of(@config.limit.to_i, false).map do |requests_batch|
       @config.if_backup ? save_and_destroy_requests_batch(requests_batch, repository) : destroy_requests_batch(requests_batch)
     end.compact
-  end
-
-  def remove_orphans
-    cases = [
-      {
-        main_model: Repository,
-        relations: [
-          {related_model: Build, fk_name: 'current_build_id'},
-          {related_model: Build, fk_name: 'last_build_id'}
-        ]
-      }, {
-        main_model: Build,
-        relations: [
-          {related_model: Repository, fk_name: 'repository_id'},
-          {related_model: Commit, fk_name: 'commit_id'},
-          {related_model: Request, fk_name: 'request_id'},
-          {related_model: PullRequest, fk_name: 'pull_request_id'},
-          {related_model: Branch, fk_name: 'branch_id'},
-          {related_model: Tag, fk_name: 'tag_id'}
-        ],
-        method: :destroy_all,
-        dry_run_complement: -> (ids) { add_builds_dependencies_to_dry_run_report(ids) }
-      }, {
-        main_model: Job,
-        relations: [
-          {related_model: Repository, fk_name: 'repository_id'},
-          {related_model: Commit, fk_name: 'commit_id'},
-          {related_model: Stage, fk_name: 'stage_id'},
-        ]
-      }, {
-        main_model: Branch,
-        relations: [
-          {related_model: Repository, fk_name: 'repository_id'},
-          {related_model: Build, fk_name: 'last_build_id'}
-        ]
-      }, {
-        main_model: Tag,
-        relations: [
-          {related_model: Repository, fk_name: 'repository_id'},
-          {related_model: Build, fk_name: 'last_build_id'}
-        ]
-      }, {
-        main_model: Commit,
-        relations: [
-          {related_model: Repository, fk_name: 'repository_id'},
-          {related_model: Branch, fk_name: 'branch_id'},
-          {related_model: Tag, fk_name: 'tag_id'}
-        ]
-      }, {
-        main_model: Cron,
-        relations: [
-          {related_model: Branch, fk_name: 'branch_id'}
-        ]
-      }, {
-        main_model: PullRequest,
-        relations: [
-          {related_model: Repository, fk_name: 'repository_id'}
-        ]
-      }, {
-        main_model: SslKey,
-        relations: [
-          {related_model: Repository, fk_name: 'repository_id'}
-        ]
-      }, {
-        main_model: Request,
-        relations: [
-          {related_model: Commit, fk_name: 'commit_id'},
-          {related_model: PullRequest, fk_name: 'pull_request_id'},
-          {related_model: Branch, fk_name: 'branch_id'},
-          {related_model: Tag, fk_name: 'tag_id'}
-        ]
-      }, {
-        main_model: Stage,
-        relations: [
-          {related_model: Build, fk_name: 'build_id'}
-        ]
-      }
-    ]
-
-    cases.each do |model_block|
-      model_block[:relations].each do |relation|
-        remove_orphans_for_table(
-          main_model: model_block[:main_model],
-          related_model: relation[:related_model],
-          fk_name: relation[:fk_name],
-          method: model_block[:method],
-          dry_run_complement: model_block[:dry_run_complement]
-        )
-      end
-    end
-  end
-
-  def add_builds_dependencies_to_dry_run_report(ids_for_delete)
-    repos_for_delete = Repository.where(current_build_id: ids_for_delete)
-    jobs_for_delete = Job.where(source_id: ids_for_delete)
-    @dry_run_reporter.add_to_report(:repositories, *repos_for_delete.map(&:id))
-    @dry_run_reporter.add_to_report(:jobs, *jobs_for_delete.map(&:id))
-  end
-
-  def remove_orphans_for_table(args)
-    main_model = args[:main_model]
-    related_model = args[:related_model]
-    fk_name = args[:fk_name]
-    method = args[:method] || :delete_all
-    dry_run_complement = args[:dry_run_complement]
-
-    main_table = main_model.table_name
-    related_table = related_model.table_name
-
-    for_delete = main_model.find_by_sql(%{
-      select a.*
-      from #{main_table} a
-      left join #{related_table} b
-      on a.#{fk_name} = b.id
-      where
-        a.#{fk_name} is not null
-        and b.id is null;
-    })
-
-    ids_for_delete = for_delete.map(&:id)
-
-    if config.dry_run
-      key = main_table.to_sym
-
-      @dry_run_reporter.add_to_report(key, *ids_for_delete)
-
-      dry_run_complement.call(ids_for_delete) if dry_run_complement
-    else
-      main_model.where(id: ids_for_delete).send(method)
-    end
   end
 
   private
---
+++ spec/backup/remove_orphans_spec.rb
@@ -0,0 +1,165 @@
+$: << 'lib'
+require 'uri'
+require 'travis-backup'
+require 'models/build'
+require 'models/job'
+require 'models/organization'
+require 'models/user'
+require 'support/factories'
+require 'support/expected_files'
+require 'support/before_tests'
+require 'pry'
+
+
+describe Backup::RemoveOrphans do
+  before(:all) do
+    BeforeTests.new.run
+  end
+
+  let(:files_location) { "dump/tests" }
+  let!(:config) { Config.new(files_location: files_location, limit: 5) }
+  let!(:connection) { DbHelper.new(config) }
+  let!(:remove_orphans) { Backup::RemoveOrphans.new(config, DryRunReporter.new) }
+
+  describe 'run' do
+    after(:all) do
+      Repository.destroy_all
+      Build.destroy_all
+      Job.destroy_all
+      Branch.destroy_all
+      Tag.destroy_all
+      Commit.destroy_all
+      Cron.destroy_all
+      PullRequest.destroy_all
+      Request.destroy_all
+      Stage.destroy_all
+    end
+
+    let!(:data) {
+      ActiveRecord::Base.connection.execute('alter table repositories disable trigger all;')
+      FactoryBot.create_list(:repository, 2)
+      FactoryBot.create_list(:build, 2)
+      FactoryBot.create_list(:job, 2)
+      FactoryBot.create_list(:branch, 2)
+      FactoryBot.create_list(:tag, 2)
+      FactoryBot.create_list(:commit, 2)
+      FactoryBot.create_list(:cron, 2)
+      FactoryBot.create_list(:pull_request, 2)
+      FactoryBot.create_list(:request, 2)
+      FactoryBot.create_list(:stage, 2)
+      FactoryBot.create_list(:repository_orphaned_on_current_build_id, 2)
+      FactoryBot.create_list(:repository_with_current_build_id, 2)
+      FactoryBot.create_list(:repository_orphaned_on_last_build_id, 2)
+      FactoryBot.create_list(:repository_with_last_build_id, 2)
+      FactoryBot.create_list(:build_orphaned_on_repository_id_with_mutually_related_repo, 2)
+      FactoryBot.create_list(:build_with_repository_id, 2)
+      FactoryBot.create_list(:build_orphaned_on_commit_id_with_mutually_related_repo, 2)
+      FactoryBot.create_list(:build_with_commit_id, 2)
+      FactoryBot.create_list(:build_orphaned_on_request_id_with_mutually_related_repo, 2)
+      FactoryBot.create_list(:build_with_request_id, 2)
+      FactoryBot.create_list(:build_orphaned_on_pull_request_id_with_mutually_related_repo, 2)
+      FactoryBot.create_list(:build_with_pull_request_id, 2)
+      FactoryBot.create_list(:build_orphaned_on_branch_id_with_mutually_related_repo, 2)
+      FactoryBot.create_list(:build_with_branch_id, 2)
+      FactoryBot.create_list(:build_orphaned_on_tag_id_with_mutually_related_repo, 2)
+      FactoryBot.create_list(:build_with_tag_id, 2)
+      FactoryBot.create_list(:job_orphaned_on_repository_id, 2)
+      FactoryBot.create_list(:job_with_repository_id, 2)
+      FactoryBot.create_list(:job_orphaned_on_commit_id, 2)
+      FactoryBot.create_list(:job_with_commit_id, 2)
+      FactoryBot.create_list(:job_orphaned_on_stage_id, 2)
+      FactoryBot.create_list(:job_with_stage_id, 2)
+      FactoryBot.create_list(:branch_orphaned_on_repository_id, 2)
+      FactoryBot.create_list(:branch_orphaned_on_last_build_id, 2)
+      FactoryBot.create_list(:branch_with_last_build_id, 2)
+      FactoryBot.create_list(:tag_orphaned_on_repository_id, 2)
+      FactoryBot.create_list(:tag_with_repository_id, 2)
+      FactoryBot.create_list(:tag_orphaned_on_last_build_id, 2)
+      FactoryBot.create_list(:tag_with_last_build_id, 2)
+      FactoryBot.create_list(:commit_orphaned_on_repository_id, 2)
+      FactoryBot.create_list(:commit_with_repository_id, 2)
+      FactoryBot.create_list(:commit_orphaned_on_branch_id, 2)
+      FactoryBot.create_list(:commit_with_branch_id, 2)
+      FactoryBot.create_list(:commit_orphaned_on_tag_id, 2)
+      FactoryBot.create_list(:commit_with_tag_id, 2)
+      FactoryBot.create_list(:cron_orphaned_on_branch_id, 2)
+      FactoryBot.create_list(:cron_with_branch_id, 2)
+      FactoryBot.create_list(:pull_request_orphaned_on_repository_id, 2)
+      FactoryBot.create_list(:pull_request_with_repository_id, 2)
+      FactoryBot.create_list(:request_orphaned_on_commit_id, 2)
+      FactoryBot.create_list(:request_with_commit_id, 2)
+      FactoryBot.create_list(:request_orphaned_on_pull_request_id, 2)
+      FactoryBot.create_list(:request_with_pull_request_id, 2)
+      FactoryBot.create_list(:request_orphaned_on_branch_id, 2)
+      FactoryBot.create_list(:request_with_branch_id, 2)
+      FactoryBot.create_list(:request_orphaned_on_tag_id, 2)
+      FactoryBot.create_list(:request_with_tag_id, 2)
+      FactoryBot.create_list(:stage_orphaned_on_build_id, 2)
+      FactoryBot.create_list(:stage_with_build_id, 2)
+      ActiveRecord::Base.connection.execute('alter table repositories enable trigger all;')
+    }
+    it 'removes orphaned repositories (with these dependent on orphaned builds)' do
+      expect { remove_orphans.run }.to change { Repository.all.size }.by -16
+    end
+
+    it 'removes orphaned builds' do
+      expect { remove_orphans.run }.to change { Build.all.size }.by -12
+    end
+
+    it 'removes orphaned jobs' do
+      expect { remove_orphans.run }.to change { Job.all.size }.by -6
+    end
+
+    it 'removes orphaned branches' do
+      expect { remove_orphans.run }.to change { Branch.all.size }.by -4
+    end
+
+    it 'removes orphaned tags' do
+      expect { remove_orphans.run }.to change { Tag.all.size }.by -4
+    end
+
+    it 'removes orphaned commits' do
+      expect { remove_orphans.run }.to change { Commit.all.size }.by -6
+    end
+
+    it 'removes orphaned crons' do
+      expect { remove_orphans.run }.to change { Cron.all.size }.by -2
+    end
+
+    it 'removes orphaned pull requests' do
+      expect { remove_orphans.run }.to change { PullRequest.all.size }.by -2
+    end
+
+    it 'removes orphaned requests' do
+      expect { remove_orphans.run }.to change { Request.all.size }.by -8
+    end
+
+    it 'removes orphaned stages' do
+      expect { remove_orphans.run }.to change { Stage.all.size }.by -2
+    end
+
+    context 'when dry run mode is on' do
+      let!(:config) { Config.new(files_location: files_location, limit: 5, dry_run: true) }
+      let!(:remove_orphans) { Backup::RemoveOrphans.new(config, DryRunReporter.new) }
+
+      before do
+        allow_any_instance_of(IO).to receive(:puts)
+      end
+
+      it 'prepares proper dry run report' do
+        remove_orphans.run
+        report = remove_orphans.dry_run_report
+        expect(report[:repositories].size).to eql 16
+        expect(report[:builds].size).to eql 12
+        expect(report[:jobs].size).to eql 6
+        expect(report[:branches].size).to eql 4
+        expect(report[:tags].size).to eql 4
+        expect(report[:commits].size).to eql 6
+        expect(report[:crons].size).to eql 2
+        expect(report[:pull_requests].size).to eql 2
+        expect(report[:requests].size).to eql 8
+        expect(report[:stages].size).to eql 2
+      end
+    end
+  end
+end
