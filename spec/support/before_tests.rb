018c38d4af334b15b8fb64620a1a2dabb0245af2
Karol Selak <karol.selak@gmail.com>
2021-10-25 13:11:59 +0200

    wip

--- spec/support/before_tests.rb
+++ spec/support/before_tests.rb
@@ -1,12 +1,12 @@
 class BeforeTests
   def run
     config = Config.new
-    puts '-------------------'
-    system("psql '#{config.database_url}' -f db/schema.sql")
-    puts '-------------------'
-    if config.destination_db_url
-      system("psql '#{config.destination_db_url}' -f db/schema.sql > /dev/null 2> /dev/null")
-    end
+    # puts '-------------------'
+    # system("psql '#{config.database_url}' -f db/schema.sql")
+    # puts '-------------------'
+    # if config.destination_db_url
+    #   system("psql '#{config.destination_db_url}' -f db/schema.sql > /dev/null 2> /dev/null")
+    # end
   end
 end
 
