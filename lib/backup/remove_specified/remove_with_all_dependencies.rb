8d7cb18a190cad2d90bc87ed8e352decf3734750
Karol Selak <karol.selak@gmail.com>
2021-11-10 14:56:52 +0100

    remove_entry_with_dependencies fixed

--- lib/backup/remove_specified/remove_with_all_dependencies.rb
+++ lib/backup/remove_specified/remove_with_all_dependencies.rb
@@ -22,24 +22,26 @@ module RemoveWithAllDependencies
   private
 
   def remove_entry_with_dependencies(model_name, id)
-    byebug
     @subfolder = "#{model_name}_#{id}_#{time_for_subfolder}"
     entry = Model.get_model(model_name).find(id)
-    hash_with_filtered = entry.ids_of_all_dependencies_with_filtered(dependencies_to_filter)
+    hash_with_filtered = entry.ids_of_all_dependencies_with_filtered(dependencies_to_filter, :without_parents)
     ids_to_remove = hash_with_filtered[:main]
     ids_to_remove.add(model_name, id)
-    ids_to_remove.join(hash_with_filtered[:filtered_out])
-    filtered_builds = hash_with_filtered[:filtered_out]&.[](:build)&.map { |id| Build.find(id) }
-    # ids_to_remove.join(*filtered_builds.map(&:ids_of_all_dependencies)) <- problem here
 
     if @config.dry_run
       @dry_run_reporter.add_to_report(ids_to_remove)
     else
-      filtered_builds&.each(&:nullify_default_dependencies)
+      nullify_filtered_dependencies(entry)
       save_id_hash_to_file(ids_to_remove) if @config.if_backup
       ids_to_remove.remove_entries_from_db(as_last: [:build])
         # order important because of foreign key constraint between builds and repos
     end
+  end
+
+  def nullify_filtered_dependencies(entry)
+    hash_with_filtered = entry.ids_of_all_dependencies_with_filtered(dependencies_to_filter)
+    filtered_builds = hash_with_filtered[:filtered_out]&.[](:build)&.map { |id| Build.find(id) }
+    filtered_builds&.each(&:nullify_default_dependencies)
   end
 
   def time_for_subfolder
