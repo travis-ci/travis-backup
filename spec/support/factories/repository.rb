fc92401963776652d85a63c426a012c4896f470d
Karol Selak Travis <karol@travis-ci.org>
2022-03-22 00:22:10 +0100

    create_for_repo in repository factory

--- spec/support/factories/repository.rb
+++ spec/support/factories/repository.rb
@@ -3,6 +3,15 @@
 require 'models/repository'
 require 'factory_bot'
 
+def create_for_repo(repository, what, how_many = 1)
+  create_list(
+    what, how_many,
+    repository_id: repository.id,
+    created_at: repository.created_at,
+    updated_at: repository.updated_at
+  )
+end
+
 FactoryBot.define do
   factory :repository do
     factory :repository_with_builds_jobs_and_logs do
@@ -68,64 +77,16 @@ FactoryBot.define do
 
     factory :repository_with_all_dependencies do
       after(:create) do |repository|
-        create(
-          :build_with_all_dependencies_and_sibling,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :request_with_all_dependencies_and_sibling,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :job_with_all_dependencies_and_sibling,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :branch_with_all_dependencies_and_sibling,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :commit_with_all_dependencies_and_sibling,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create_list(
-          :ssl_key, 2,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create_list(
-          :permission, 2,
-          repository_id: repository.id
-        )
-        create_list(
-          :star, 2,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :pull_request_with_all_dependencies_and_sibling,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :tag_with_all_dependencies_and_sibling,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
+        create_for_repo(repository, :build_with_all_dependencies_and_sibling)
+        create_for_repo(repository, :request_with_all_dependencies_and_sibling)
+        create_for_repo(repository, :job_with_all_dependencies_and_sibling)
+        create_for_repo(repository, :branch_with_all_dependencies_and_sibling)
+        create_for_repo(repository, :commit_with_all_dependencies_and_sibling)
+        create_for_repo(repository, :ssl_key, 2)
+        create_list(:permission, 2, repository_id: repository.id)
+        create_for_repo(repository, :star, 2)
+        create_for_repo(repository, :pull_request_with_all_dependencies_and_sibling)
+        create_for_repo(repository, :tag_with_all_dependencies_and_sibling)
       end
 
       factory :repository_with_all_dependencies_and_sibling do
@@ -139,32 +100,12 @@ FactoryBot.define do
         updated_at { 12.months.ago.to_time.utc }
 
         after(:create) do |repository|
-          create(
-            :build_for_removing_heavy_data,
-            repository_id: repository.id,
-            created_at: repository.created_at,
-            updated_at: repository.updated_at
-          )
-          create(
-            :build_for_removing_heavy_data,
-            repository_id: repository.id
-          )
-          last_build = create(
-            :build_for_removing_heavy_data,
-            repository_id: repository.id,
-            created_at: repository.created_at,
-            updated_at: repository.updated_at
-          )
-          create(
-            :request,
-            repository_id: repository.id,
-            created_at: repository.created_at,
-            updated_at: repository.updated_at
-          )
-          create(
-            :request,
-            repository_id: repository.id
-          )
+          create_for_repo(repository, :build_for_removing_heavy_data)
+          create(:build_for_removing_heavy_data, repository_id: repository.id)
+          last_build = create_for_repo(repository, :build_for_removing_heavy_data).last
+          create_for_repo(repository, :request)
+          create(:request, repository_id: repository.id)
+
           repository.update(last_build_id: last_build.id)
         end
       end
@@ -172,65 +113,18 @@ FactoryBot.define do
 
     factory :repository_with_safe_dependencies do
       after(:create) do |repository|
-        create(
-          :build,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :request,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :job,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :branch,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :commit,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :ssl_key,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :permission,
-          repository_id: repository.id
-        )
-        create(
-          :star,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :pull_request,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
-        create(
-          :tag,
-          repository_id: repository.id,
-          created_at: repository.created_at,
-          updated_at: repository.updated_at
-        )
+        create_for_repo(repository, :build)
+        create_for_repo(repository, :request)
+        create_for_repo(repository, :job)
+        create_for_repo(repository, :branch)
+        create_for_repo(repository, :commit)
+        create_for_repo(repository, :ssl_key)
+        create(:permission, repository_id: repository.id)
+        create_for_repo(repository, :star)
+        create_for_repo(repository, :pull_request)
+        create_for_repo(repository, :tag)
       end
+
       factory :repository_with_safe_dependencies_and_sibling do
         after(:create) do |repository|
           create(:repository, repository.attributes_without_id.symbolize_keys)
