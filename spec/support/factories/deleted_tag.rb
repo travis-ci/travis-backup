0967b9579d0a19ea94c5b6b738bd2ba82d7514ca
Karol Selak Travis <karol@travis-ci.org>
2022-03-30 16:24:53 +0200

    comments removed

--- spec/support/factories/deleted_tag.rb
+++ spec/support/factories/deleted_tag.rb
@@ -5,12 +5,6 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :deleted_tag do
-    # repository_id { 1 }
-    # name { 'abc' }
-    # last_build_id { 1 }
-    # exists_on_github { false }
-    # org_id { 1 }
-    # com_id { 1 }
     sequence(:id) { |n| n }
   end
 end
